dc87118d0175c4f08ea37d408ffa81bc517276b7
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-10-07 17:22:05 -0300

    aula

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidae do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitada. MULTADO`)
+}
+else{
+    console.log(`Dirija sempre com segurança usando cinto de segurança`)
+}
