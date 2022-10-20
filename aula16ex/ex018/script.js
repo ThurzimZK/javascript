911a55e85e6370a2d516c6a1b0cb0d74793b4989
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-10-20 18:58:30 -0300

    fim

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,41 +1,62 @@
-function adicionar(){
-    let numero = document.getElementById('inum')
-    var lista = document.getElementById('sellista')
+let numero = document.getElementById('inum')
+let lista = document.getElementById('sellista')
+let res = document.getElementById('res')
+let valores = []
 
-    if (numero.value.length == 0 || numero.value <= 0 || numero.value >= 101 ){
-        alert('[ERRO] Verifique os dados e tente novamente!')
-        lista.innerHTML = ''
-    } else {
-        var n = Number(numero.value)
-        lista.innerHTML = ''
-        
-        let item = document.createElement('option')
-        item.text += `Valor ${n} adicionado`
-        item.value += `tab${n}`
-        lista.appendChild(item)
-        
+function isNum(n){
+    if(Number(n) >= 1 && Number(n) <=100){
+        return true
+    } else{
+        return false
     }
 }
 
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    } else{
+        return false
+    }
+}
+
+function adicionar(){
+   if(isNum(numero.value) && !inLista(numero.value, valores)){
+        valores.push(Number(numero.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${numero.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+   } else{
+        alert('Valor inválido ou já encontrado na lista.')
+   }
+   numero.value = ''
+   numero.focus()
+}
+
 function finalizar(){
-    let resultado = document.getElementById('res')
-    resultado.innerHTML = ''
-    let lista = document.getElementById('sellista')
-    
-    let tamanho = lista.length
-   
-    const array = lista; 
-    let soma = 0; 
-    for (let i = 0; i < array. length; i++) { soma += array[i]; }
-    
-    // total de numeros
-    resultado.innerHTML += `Ao lado, temos ${lista.length - 1} números cadastrados<br>`
-    // maior valor
-    resultado.innerHTML += `O maior valor informado foi ${tamanho}.<br>`
-    // menor valor
-    resultado.innerHTML += `O menor valor informado foi ${tamanho}.<br>`
-    // soma de todos os valores
-    resultado.innerHTML += `Somando todos os valores, temos ${soma}.<br>`
-    // media de todos os valores 
-    resultado.innerHTML += `A média dos vaores digitados é ${tamanho}.`
+  if(valores.length == 0){
+    alert('Adicione valores antes de finalizar')
+  } else{
+    let tot = valores.length
+    valores.sort()
+    let soma = 0
+    let maior = valores[0]
+    let menor = valores[0]
+    for(let pos in valores){
+        soma += valores[pos]
+        if(valores[pos] > maior){
+            maior = valores[pos]
+        }
+        if(valores[pos] < menor){
+            menor = valores[pos]
+        }
+    }
+    media = soma / tot
+    res.innerHTML = ''
+    res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados. </p>`
+    res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+    res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+    res.innerHTML += `<p>A soma dos valores informados é ${soma}</p>`
+    res.innerHTML += `<p>A média dos valores informados é ${media}</p>`
+  }
 }
