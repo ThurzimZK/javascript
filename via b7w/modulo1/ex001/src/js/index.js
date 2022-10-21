d06df789ca3b1d3b9218446008b87103c6ef9323
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-10-21 17:51:22 -0300

    modulo 1

---
+++ via b7w/modulo1/ex001/src/js/index.js
@@ -0,0 +1,7 @@
+let nome = 'Arthur' // string
+let idade = 18 // number
+let logado = true //boleano
+let lista = ['ovo', 'farinha', 'leite'] // array
+let cadastro = {nome: 'Arthur', idade: 18, sexo: 'M'} // object
+
+console.log(typeof cadastro.nome)
---
+++ via b7w/modulo1/ex002/index.js
@@ -0,0 +1,11 @@
+// ex 1
+let carro = 'Ferrari'
+console.log(carro)
+
+// ex 2
+let bolo = 19.99
+console.log(`O valor do bolo é R$${bolo}`)
+
+// ex 3
+let cidade = 'São Paulo'
+console.log(cidade)
