2ad4f0d20c68f9c8f8137c093168418b4189d017
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-11-04 18:32:00 -0300

    modulo5

---
+++ via b7w/modulo5/ex001/script.js
@@ -0,0 +1,16 @@
+let nome = 'thurzim da loud'
+
+let resultado = ''
+if(nome.indexOf('silva') > -1){
+    resultado = 'Achou'
+} else{
+    resultado = 'Não achou'
+}
+
+console.log(nome.length);
+
+console.log(nome.indexOf('coda'));
+console.log(nome.indexOf('t'));
+console.log(nome.indexOf(' '));
+
+console.log(resultado);
