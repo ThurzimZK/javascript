5d8972a769f4b5c17ea1992f5995c4263018bdc9
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-11-04 18:00:47 -0300

    modulo4

--- via b7w/modulo4/ex002/index.js
+++ via b7w/modulo4/ex002/index.js
@@ -1,1 +1,18 @@
+function subirTela(){
+    //window.scrollTo(0, 0)
+    window.scrollTo({
+        top: 0,
+        //left: 0,
+        behavior: 'smooth'
+    })
+}
 
+function posicaoScroll(){
+    if(window.scrollY === 0){
+        document.querySelector('.scrollbutton').style.display = 'none'
+    } else{
+        document.querySelector('.scrollbutton').style.display = 'block'
+    }
+}
+
+window.addEventListener('scroll', posicaoScroll)
