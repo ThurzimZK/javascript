5fedffe1a861c1c7f38894ff23604183d1cb4d81
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-11-07 17:14:45 -0300

    modulo5

---
+++ via b7w/modulo5/ex003.js
@@ -0,0 +1,16 @@
+//let nome = 'Arthur Oliveira haha'
+let nome = '1,2,3,4,5,6,7,8,9,10'
+
+let resultado = nome.split(',')
+//nome[3] msm coisa
+//substr(3, 1) posição e quantidade de item apartir dela
+//charAt(3) mostra qual caracter esta nessa posição
+//trim() tira os espaços trim().length pra saber o tamanho real
+//concat(' Oliveira') inutil
+//toLowerCase() minusculo
+//toUpperCase() maiusculo
+//replace('carros', 'motos') trocar um por outro
+
+
+console.log('nome: ', nome);
+console.log('resultado: ',resultado);
