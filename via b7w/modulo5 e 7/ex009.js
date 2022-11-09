8fe895f3013b82e1282cbbb180d592fd0eeb28f6
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-11-09 17:29:04 -0300

    modulo5e7

---
+++ via b7w/modulo5 e 7/ex009.js
@@ -0,0 +1,8 @@
+//let d = new Date(2022, 1, 11, 12, 30, 12) // ano, mes, dia, hora, minuto, segundo
+//let d = new Date('2004-02-11 15:42:17')
+let d = new Date(1667999999999)// hj
+//let d = new Date(-1667999999999)
+
+//console.log(d.toDateString());
+console.log(d.toUTCString());
+//console.log(d.toString());
---
+++ via b7w/modulo5 e 7/ex010.js
@@ -0,0 +1,13 @@
+let d = new Date()
+
+//let res = Date.now()
+let res = d
+
+//getMinutes()
+//getHours()
+//getDate() dia
+//getDay() dia da semana de 0(domingo) a 6(sabado)
+//getMonth() de 0 a 11
+//getFullYear()
+
+console.log(res);
