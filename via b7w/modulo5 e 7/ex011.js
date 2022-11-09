d34af91d67eeffc5d33146cec18d77da15508963
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-11-09 18:20:21 -0300

    modulo5e7

---
+++ via b7w/modulo5 e 7/ex011.js
@@ -0,0 +1,10 @@
+let d = new Date()
+
+d.setHours( d.getHours() - 3 )
+//setDate( d.getDate() + 5 )
+//setDate(14)
+//setMonth(1)
+//setFullYear(2020)
+
+let res = d
+console.log(res);
---
+++ via b7w/modulo5 e 7/ex012.js
@@ -0,0 +1,13 @@
+let novoValor = Math.
+//floor(Math.random() * 10) combinações para o aleatorio ser entre 0 e outro numero
+//random() aleatorio de 0 a 1
+//max(7, 18, 2, 21, 6, 120) maior
+//min(7, 18, 2, 21, 6, 120) menor
+//abs( -9.3532 ) transforma em positivo 
+//ceil(2.1) arredonda sempre pra cima
+//floor(2.9) arredonda sempre pra baixo
+//round(2.5) arredonda pro mais proximo
+//Math.PI.toFixed(2) pi - com 2 casa dps da ,
+
+
+console.log(novoValor);
