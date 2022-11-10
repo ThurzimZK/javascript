b2f8920ce1de48d79588dc9cb6a22afbdb74c12b
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-11-10 18:53:46 -0300

    modulo5e7

---
+++ via b7w/modulo5 e 7/ex020.js
@@ -0,0 +1,14 @@
+let lista = ['ovo', 'cafe', 'arroz', 'feijao', 'macarrao']
+
+console.log(lista.includes('ovo')); // true or false
+
+//===========================================
+
+let nome = 'Arthur'
+let n = nome.toUpperCase()
+
+console.log(n.includes('a'.toUpperCase()));
+
+//==========================================
+
+console.log('oi '.repeat(20));
---
+++ via b7w/modulo5 e 7/ex021.js
@@ -0,0 +1,16 @@
+let lista = ['ovo', 'macarrao', 'feijao', 'pipoca']
+
+/*console.log(Object.keys(lista));
+console.log(Object.values(lista));
+console.log(Object.entries(lista));*/
+
+//================================
+
+let pessoa = {
+    nome: 'Arthur',
+    sobrenome: 'Oliveira',
+    idade: 18
+}
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+console.log(Object.entries(pessoa));
