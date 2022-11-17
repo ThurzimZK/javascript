98e0635379147601ae7028ece6e5d29c6b124d80
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-11-17 18:56:18 -0300

    modulo8

---
+++ via b7w/modulo8/ex005/src/js/index.js
@@ -0,0 +1,13 @@
+function loadPosts() {
+    document.getElementById('posts').innerHTML = 'Carregando...'
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(function(resultado){
+            return resultado.json()
+        })
+        .then(function(json){
+            document.getElementById('posts').innerHTML = json.length+' posts'
+        })
+        .catch(function(error){
+            console.log('deu problema');
+        })
+}
