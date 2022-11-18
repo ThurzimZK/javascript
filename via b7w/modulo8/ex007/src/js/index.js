4cfd12b9eb98268b102d00df306a7a4d5c426a87
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-11-18 18:16:00 -0300

    modulo8

--- via b7w/modulo8/ex007/src/js/index.js
+++ via b7w/modulo8/ex007/src/js/index.js
@@ -1,4 +1,15 @@
-function enviar() {
-    let arquivo = document.getElementById('arquivo').files
-    console.log(arquivo);
+async function enviar() {
+    let arquivo = document.getElementById('arquivo').files[0] //files[0] quando for 1 arquivo apenas
+
+    let body = new FormData()
+    body.append('title', 'bla bla bla')
+    body.append('arquivo', arquivo)
+
+    let req = await fetch('https://www.sitedothurzim.com.br/upload', {
+        method: 'POST',
+        body: body,
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })
 }
