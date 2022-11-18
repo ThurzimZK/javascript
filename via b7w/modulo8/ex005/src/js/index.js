6a141073b0aee2e4e9e5dbf827c8f1fbcbacc818
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2022-11-18 16:34:36 -0300

    modulo8

--- via b7w/modulo8/ex005/src/js/index.js
+++ via b7w/modulo8/ex005/src/js/index.js
@@ -1,13 +1,34 @@
-function loadPosts() {
+/*let loadPosts = async () =>{
+
+}*/
+async function loadPosts() {
     document.getElementById('posts').innerHTML = 'Carregando...'
+
+    let req = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let json = await req.json()
+
+    montarBlog(json)
+
+/* //mesma coisa
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(function(resultado){
             return resultado.json()
         })
         .then(function(json){
-            document.getElementById('posts').innerHTML = json.length+' posts'
+            montarBlog(json)
         })
         .catch(function(error){
             console.log('deu problema');
-        })
+        })*/
+}
+
+function montarBlog(lista){
+    let html = ''
+    
+    for(let i in lista){
+        html += '<h3>'+lista[i].id+' - '+lista[i].title+'</h3>'
+        html += lista[i].body+'<br>'
+        html += '<hr>'
+        document.getElementById('posts').innerHTML = html
+    }
 }
---
+++ via b7w/modulo8/ex007/src/js/index.js
@@ -0,0 +1,4 @@
+function enviar() {
+    let arquivo = document.getElementById('arquivo').files
+    console.log(arquivo);
+}
