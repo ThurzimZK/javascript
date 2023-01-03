bc9fab1bba6a962e3a4bdc29784dd7a44de72df9
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2023-01-03 18:43:09 -0300

    update

--- via b7w/modulo4/ex006.js
+++ via b7w/modulo4/ex006.js
@@ -7,3 +7,12 @@ class Person {
     }
 
 }
+
+function createPerson(name, age) {
+    let p = new Person(name)
+    p.age = age
+    return p
+}
+
+let p1 = createPerson('Arthur', 18)
+console.log(`${p1.name} tem ${p1.age} anos`);
