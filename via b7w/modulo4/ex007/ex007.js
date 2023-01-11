f53a12b6fb804be34a9b3320000857644e251e41
Arthur <109552351+ThurzimZK@users.noreply.github.com>
2023-01-11 18:30:18 -0300

    update

--- via b7w/modulo4/ex007/ex007.js
+++ via b7w/modulo4/ex007/ex007.js
@@ -1,3 +1,28 @@
+class Form{
+    items = []
+    method = 'GET'
+    constructor(container, method, action) {
+        this.container = document.querySelector(container)
+        this.method = method
+        this.action = action
+    }
+
+    addItem(item) {
+        this.items.push(item)
+    }
+
+    render() {
+        let formElement = document.createElement('form')
+        formElement.setAttribute('method', this.method)
+        formElement.setAttribute('action', this.action)
+
+        for(let i in this.items){
+            this.items[i].render(formElement)
+        }
+
+        this.container.appendChild(formElement)
+    }
+}
 class Input {
 
     _type = 'text'
@@ -19,15 +44,49 @@ class Input {
             throw new Error(`Input '${t} type doesn't exist.'`)
         }
     }
+
+    render(formElement) {
+        let el = document.createElement('input')
+        el.type = this.type
+        el.name = this.name
+        el.placeholder = this.label
+        el.required = this.required
+        formElement.appendChild(el)
+    }
+}
+
+class Button extends Input {
+    constructor(label){
+        super('', label)
+        this._type = 'submit'
+    }
+
+    render(formElement) {
+        let el = document.createElement('input')
+        el.type = this.type
+        el.value = this.label
+        formElement.appendChild(el)
+    }
 }
 
 // IMPLEMENTAÇÃO
+
+// FORMULARIO
+let form = new Form('.formArea', 'POST', 'https://site.com.br')
 
 // EMAIL
 let email = new Input('email', 'Digite seu e-mail')
 email.type = 'email'
 email.required = true
+form.addItem(email)
 
 // SENHA
 let password = new Input('password', 'Digite sua senha')
 password.type = 'password'
+form.addItem(password)
+
+// BOTAO
+let button = new Button ('Enviar')
+form.addItem(button) // console.log(form.items) na linha de baixo pra ver quantos items tem
+
+form.render()
